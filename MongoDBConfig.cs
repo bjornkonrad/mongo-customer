f033c6a14cc2c3d8d9322b0954f389a96c033a82
Björn Wahlberg <62355836+bjornkonrad@users.noreply.github.com>
2022-12-09 07:54:16 +0100

    Upload proj

---
+++ MongoDBConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace Customer
+{
+    public class MongoDBConfig
+    {
+        public string Database
+        {
+            get
+            {
+                return "Customers";
+            }
+        }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string ConnectionString
+        {
+            get
+            {
+                return $@"mongodb://root:example@localhost:27017";
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using Customer.Model;
+using Customer.Repository;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var context = new PersonContext(new Customer.MongoDBConfig());
+        var collection = new PersonCollection(context);
+
+        //var usAddress = new Address("Same as Susan", "2nd", "12345", "Vegas", "US of A");
+        //usAddress.State = "Nevada";
+
+        //var ukAddress = new Address("Bromyard road", "2nd", "12345", "Worcester", "UK");
+        //ukAddress.County = "Worcestershire";
+
+        //var person = new Person("Luca", usAddress);
+        //person.Addresses.Add(ukAddress);
+
+        var seAddress = new Address("Sköldgatan 5", "6:e", "11863", "Stockholm", "Sverige");
+        seAddress.Municipality = "Stockholm";
+
+        var person = new Person("Min granne", seAddress);
+        var repository = new PersonRepository(context);
+        await repository.Create(person);
+
+        collection.OutPutPersons();
+
+        Console.WriteLine("Hello World!");
+    }
+}
